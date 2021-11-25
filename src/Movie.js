20e884d1240e467f7c9354b2f926a1488c347de1
prasanthk99 <prasanthgayu1999@gmail.com>
2021-11-25 21:11:03 +0530

    Some style changes

--- src/Movie.js
+++ src/Movie.js
@@ -38,49 +38,50 @@ function Movie() {
         <>
             <Navbar setData={setDatas} />
             <div className="content">
-                {datas.map((data, index) => {
-                    return (
-                        <div key={index} className="Movies">
-                            {(loading) ? <h1>Loading</h1> : data.map((da) => {
-                                const { id, title, poster_path, vote_average, overview, release_date } = da;
-                                let year;
-                                if (release_date) {
-                                    year = release_date.substr(0, 4);
-                                } else {
-                                    year = "";
-                                }
-                                let votecolor = "";
-                                if (vote_average <= 5) {
-                                    votecolor = "red";
-                                }
-                                else if (vote_average > 5) {
-                                    votecolor = "orange";
-                                }
-                                else if (vote_average > 8) {
-                                    votecolor = "green";
-                                }
+                {(loading) ? <h1>Loading...</h1> :
+                    datas.map((data, index) => {
+                        return (
+                            <div key={index} className="Movies">
+                                {(loading) ? <h1>Loading</h1> : data.map((da) => {
+                                    const { id, title, poster_path, vote_average, overview, release_date } = da;
+                                    let year;
+                                    if (release_date) {
+                                        year = release_date.substr(0, 4);
+                                    } else {
+                                        year = "";
+                                    }
+                                    let votecolor = "";
+                                    if (vote_average <= 5) {
+                                        votecolor = "red";
+                                    }
+                                    else if (vote_average > 5) {
+                                        votecolor = "orange";
+                                    }
+                                    else if (vote_average > 8) {
+                                        votecolor = "green";
+                                    }
 
-                                let vote = vote_average;
-                                return (
-                                    <div className="Movie" key={id}>
-                                        <div className="img-div">
-                                            <img src={(!poster_path) ? "https://propertywiselaunceston.com.au/wp-content/themes/property-wise/images/no-image@2x.png" : image_API + poster_path} alt={title} className="image" />
+                                    let vote = vote_average;
+                                    return (
+                                        <div className="Movie" key={id}>
+                                            <div className="img-div">
+                                                <img src={(!poster_path) ? "https://propertywiselaunceston.com.au/wp-content/themes/property-wise/images/no-image@2x.png" : image_API + poster_path} alt={title} className="image" />
+                                            </div>
+                                            <div className="info">
+                                                <h1>{title} (<span>{year}</span>)</h1>
+                                                <p style={{ color: `${votecolor}` }}>{vote}</p>
+                                            </div>
+                                            <div className="overviewtxt">
+                                                <h1>{title} (<span>{year}</span>)</h1>
+                                                <p>{overview}</p>
+                                            </div>
                                         </div>
-                                        <div className="info">
-                                            <h1>{title} (<span>{year}</span>)</h1>
-                                            <p style={{ color: `${votecolor}` }}>{vote}</p>
-                                        </div>
-                                        <div className="overviewtxt">
-                                            <h1>{title} (<span>{year}</span>)</h1>
-                                            <p>{overview}</p>
-                                        </div>
-                                    </div>
-                                )
-                            }
-                            )}
-                        </div>
-                    )
-                })
+                                    )
+                                }
+                                )}
+                            </div>
+                        )
+                    })
                 }
                 <button className="loadmore-btn" onClick={() => loadMore()}>Load More</button>
             </div>
