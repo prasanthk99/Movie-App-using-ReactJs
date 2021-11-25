9b235fea54643f02f6889720659139524b966c9d
prasanthk99 <prasanthgayu1999@gmail.com>
2021-11-25 20:55:10 +0530

    Added More Pages

--- src/Movie.js
+++ src/Movie.js
@@ -3,67 +3,86 @@ import Navbar from "./NavBar";
 import "./App.css";
 
 const image_API = "https://image.tmdb.org/t/p/w1280";
-const feature = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+// const feature = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=";
+
+let page = 1;
 
 function Movie() {
     const [datas, setDatas] = useState([]);
     const [loading, setloading] = useState(true);
 
-    useEffect(() => {
-        fetch(feature)
+    const getfetch = () => {
+        let url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=${page}`;
+        fetch(url)
             .then((res) => res.json())
             .then((D) => {
-                setDatas(D.results);
+                setDatas([...datas, D.results]);
                 setloading(false);
             })
+
+    }
+
+    useEffect(() => {
+        getfetch();
     }, []);
 
-    console.log(datas);
 
+    const loadMore = () => {
+        page += 1;
+        getfetch();
+    }
+
+    console.log(datas);
 
     return (
         <>
             <Navbar setData={setDatas} />
-            <div className="Movies">
-                {((loading) ? <h1>Loading...</h1> : datas.map((data) => {
-                    const { id, title, poster_path, vote_average, overview, release_date } = data;
-                    let year;
-                    if (release_date) {
-                        year = release_date.substr(0, 4);
-                    } else {
-                        year = "";
-                    }
-                    let votecolor = "";
-                    if (vote_average <= 5) {
-                        votecolor = "red";
-                    }
-                    else if (vote_average > 5) {
-                        votecolor = "orange";
-                    }
-                    else if (vote_average > 8) {
-                        votecolor = "green";
-                    }
-
-                    let vote = vote_average.toFixed(1);
-
-
+            <div className="content">
+                {datas.map((data, index) => {
                     return (
-                        <div className="Movie" key={id}>
-                            <div className="img-div">
-                                <img src={(!poster_path) ? "https://propertywiselaunceston.com.au/wp-content/themes/property-wise/images/no-image@2x.png" : image_API + poster_path} alt={title} className="image" />
-                            </div>
-                            <div className="info">
-                                <h1>{title} (<span>{year}</span>)</h1>
-                                <p style={{ color: `${votecolor}` }}>{vote}</p>
-                            </div>
-                            <div className="overviewtxt">
-                                <h1>{title} (<span>{year}</span>)</h1>
-                                <p>{overview}</p>
-                            </div>
+                        <div key={index} className="Movies">
+                            {(loading) ? <h1>Loading</h1> : data.map((da) => {
+                                const { id, title, poster_path, vote_average, overview, release_date } = da;
+                                let year;
+                                if (release_date) {
+                                    year = release_date.substr(0, 4);
+                                } else {
+                                    year = "";
+                                }
+                                let votecolor = "";
+                                if (vote_average <= 5) {
+                                    votecolor = "red";
+                                }
+                                else if (vote_average > 5) {
+                                    votecolor = "orange";
+                                }
+                                else if (vote_average > 8) {
+                                    votecolor = "green";
+                                }
+
+                                let vote = vote_average;
+                                return (
+                                    <div className="Movie" key={id}>
+                                        <div className="img-div">
+                                            <img src={(!poster_path) ? "https://propertywiselaunceston.com.au/wp-content/themes/property-wise/images/no-image@2x.png" : image_API + poster_path} alt={title} className="image" />
+                                        </div>
+                                        <div className="info">
+                                            <h1>{title} (<span>{year}</span>)</h1>
+                                            <p style={{ color: `${votecolor}` }}>{vote}</p>
+                                        </div>
+                                        <div className="overviewtxt">
+                                            <h1>{title} (<span>{year}</span>)</h1>
+                                            <p>{overview}</p>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                            )}
                         </div>
                     )
-                }))
+                })
                 }
+                <button className="loadmore-btn" onClick={() => loadMore()}>Load More</button>
             </div>
         </>
     )
